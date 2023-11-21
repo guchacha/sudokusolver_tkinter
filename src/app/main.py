81a84732fc57956f14df896b0cd449ca2c379d74
Agnieszka Kowalczyk <gu.nowakowska@gmail.com>
2023-11-21 14:01:06 +0100

    [backend](refactor) update entry point file  correct error with saving own unsolved puzzle to list, combine 2 function preparing random puzzle into 1 function, combine 2 function solving puzzle into 1 function, update descriptions of functions

--- src/app/main.py
+++ src/app/main.py
@@ -69,99 +69,41 @@ puzzle7 = [[0, 1, 9, 0, 6, 0, 5, 4, 0],
            [0, 8, 3, 0, 4, 0, 7, 1, 0]]
 
 
-def random_puzzle() -> list:
+def random_puzzle():
     """
-    Function selecting random puzzle from group of predefined puzzles.
-    :return: 9x9 2d list of integers with sudoku puzzle ready to solve, unknown values as number 0
-    """
-
-    # random puzzle selection
-    return random.choice([puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7])
-
-
-def sudoku(puzzle: list) -> list:
-    """
-    Function solving sudoku puzzle.
-    :param puzzle: 9x9 2d list of integers with sudoku puzzle to solve, unknown values as number 0
-    :return: 9x9 2d list of integers with solved sudoku puzzle
+    Function selects random puzzle from group of predefined puzzles and showing it in the window. Function also
+    defines button to pass unsolved puzzle to solving function.
     """
 
-    # information about no progress in solution
-    global no_progress
-    no_progress = 0
-
-    # converting input 2d list with puzzle into 2d numpy array and calculating the number of unknowns
-    grid = np.array(puzzle)
-    ##print(f"Puzzle to solve:\n{grid}")
-    zeros = np.count_nonzero(grid == 0)
-    ##print(f"Number of unknowns: {zeros}\n")
-
-    # loop depending on the remaining number of unknowns
-    round_counter = 1
-    while zeros > 0:
-        ##print(f"---Round {round_counter}---")
-
-        # loop through every 3x3 box and every value in 9x9 grid, checking if value is in box
-        for box_row in [0, 3, 6]:
-            for box_col in [0, 3, 6]:
-                for value in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-                    box = np.copy(grid[box_row:box_row + 3, box_col:box_col + 3])
-                    if value not in box:
-
-                        # loop through every row in 3x3 box and crossing out rows with checking value
-                        for square_row in [box_row, box_row + 1, box_row + 2]:
-                            if value in grid[square_row, 0:9]:
-                                box[square_row - box_row, :] = [10, 10, 10]
-
-                        # loop through every column in 3x3 box and crossing out columns with checking value
-                        for square_col in [box_col, box_col + 1, box_col + 2]:
-                            if value in grid[0:9, square_col]:
-                                box[:, square_col - box_col] = [10, 10, 10]
-
-                        # inserting value in the place of unknown if the remaining number of unknowns in 3x3 box
-                        # after crossing out is equal to 1
-                        if np.count_nonzero(box == 0) == 1:
-                            zero_pos = np.argwhere(box == 0)
-                            grid[zero_pos[0][0] + box_row, zero_pos[0][1] + box_col] = value
-                            ##print(f"The value: {value} on the position: {zero_pos[0]}")
-
-        # calculating the remaining number of unknowns
-        prev_zeros = zeros
-        ##print(f"Previous number of unknowns: {prev_zeros}")
-        zeros = np.count_nonzero(grid == 0)
-        ##print(f"Remaining number of unknowns: {zeros}\n")
-
-        # breaking the loop if no progress, probably too many unknowns to have one version of solution/incorrect input
-        if prev_zeros == zeros:
-            ##print(f"No progress, number of unknowns: {zeros}, incorrect input")
-            no_progress = 1
-            break
-
-        round_counter = round_counter + 1
-
-    # solution printing and converting into 2d list
-    ##print(f"Puzzle solved:\n{grid}")
-    return grid.tolist()
-
-
-def show_random():
+    # cleaning bottom frame of window
     for widget in bot_frame.winfo_children():
         widget.destroy()
 
-    unsolved_list = random_puzzle()
+    # random puzzle selection
+    unsolved_list = random.choice([puzzle1, puzzle2, puzzle3, puzzle4, puzzle5, puzzle6, puzzle7])
+
+    # showing random puzzle
     unsolved_str = '\n'.join(' '.join(str(x) for x in row) for row in unsolved_list)
     unsolved_lab = tk.Label(bot_frame, text=unsolved_str, font=('Arial', 18))
     unsolved_lab.grid(row=0, column=0)
 
+    # button which caused passing unsolved puzzle to solving function
     solve_btn = tk.Button(bot_frame, text="Solve this puzzle", width=20, height=1, font=('Arial', 18),
                           command=lambda: solve_puzzle(unsolved_list))
     solve_btn.grid(row=1, column=0, padx=5, pady=5)
 
 
-def input_own():
+def own_puzzle():
+    """
+    Function shows 9x9 grid of entry boxes in the window. Function defines button to pass users inputs to checking
+    function.
+    """
+
+    # cleaning bottom frame of window
     for widget in bot_frame.winfo_children():
         widget.destroy()
 
+    # 9x9 grid of entry boxes inserted into 2d list
     outer_list_entry = []
     for r in range(9):
         inner_list_entry = []
@@ -171,13 +113,20 @@ def input_own():
             inner_list_entry.append(box_entry)
         outer_list_entry.append(inner_list_entry)
 
+    # button which caused passing users inputs to checking function
     check_btn = tk.Button(bot_frame, text="Check and save your puzzle", width=25, font=('Arial', 18),
                           command=lambda: check_puzzle(outer_list_entry))
     check_btn.grid(row=9, column=0, columnspan=9, padx=5, pady=5)
 
 
-def check_puzzle(outer_list_entry):
-    # creation of solving button, setting it to normal state
+def check_puzzle(outer_list_entry: list):
+    """
+    Function checks correction of single input, single row, single column and single 3x3 box of inputs. Function defines
+    button to pass users inputs to solving function. The button is set in disabled state if users inputs are incorrect.
+    :param outer_list_entry: 9x9 2d list of tkinter entry objects with sudoku puzzle to solve
+    """
+
+    # button which caused passing unsolved puzzle to solving function, setting it to normal state
     solve_btn = tk.Button(bot_frame, text="Solve this puzzle", width=25, height=1, font=('Arial', 18),
                           command=lambda: solve_puzzle(unsolved_list))
     solve_btn.grid(row=10, column=0, columnspan=9, padx=5, pady=5)
@@ -199,6 +148,7 @@ def check_puzzle(outer_list_entry):
             elif square_str == "":
                 inner_unsolved_list.append(0)
             else:
+                inner_unsolved_list.append(0)
                 mes1 = f"Incorrect input in {count_row} row and {count_col} column.\n" \
                        f"You can input single number from 1 to 9 or leave empty box."
                 messagebox.showerror(title="Incorrect input", message=mes1)
@@ -262,20 +212,82 @@ def check_puzzle(outer_list_entry):
                 solve_btn['state'] = tk.DISABLED
 
 
-def solve_puzzle(unsolved_list):
-    solved_list = sudoku(unsolved_list)
+def solve_puzzle(unsolved_list: list):
+    """
+    Function solves sudoku puzzle and shows solution in the window. In case of no progress (too many unknowns) function
+    breaks and shows message box with warning.
+    :param unsolved_list: 9x9 2d list of integers with sudoku puzzle to solve, unknown values as number 0.
+    """
+
+    # converting input 2d list with puzzle into 2d numpy array and calculating the number of unknowns
+    grid = np.array(unsolved_list)
+    # print(f"Puzzle to solve:\n{grid}") ###
+    zeros = np.count_nonzero(grid == 0)
+    # print(f"Number of unknowns: {zeros}\n") ###
+
+    # loop depending on the remaining number of unknowns
+    round_counter = 1
+    while zeros > 0:
+        # print(f"---Round {round_counter}---") ###
+
+        # loop through every 3x3 box and every value in 9x9 grid, checking if value is in box
+        for box_row in [0, 3, 6]:
+            for box_col in [0, 3, 6]:
+                for value in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                    box = np.copy(grid[box_row:box_row + 3, box_col:box_col + 3])
+                    if value not in box:
+
+                        # loop through every row in 3x3 box and crossing out rows with checking value
+                        for square_row in [box_row, box_row + 1, box_row + 2]:
+                            if value in grid[square_row, 0:9]:
+                                box[square_row - box_row, :] = [10, 10, 10]
+
+                        # loop through every column in 3x3 box and crossing out columns with checking value
+                        for square_col in [box_col, box_col + 1, box_col + 2]:
+                            if value in grid[0:9, square_col]:
+                                box[:, square_col - box_col] = [10, 10, 10]
+
+                        # inserting value in the place of unknown if the remaining number of unknowns in 3x3 box
+                        # after crossing out is equal to 1
+                        if np.count_nonzero(box == 0) == 1:
+                            zero_pos = np.argwhere(box == 0)
+                            grid[zero_pos[0][0] + box_row, zero_pos[0][1] + box_col] = value
+                            # print(f"The value: {value} on the position: {zero_pos[0]}") ###
+
+        # calculating the remaining number of unknowns
+        prev_zeros = zeros
+        # print(f"Previous number of unknowns: {prev_zeros}") ###
+        zeros = np.count_nonzero(grid == 0)
+        # print(f"Remaining number of unknowns: {zeros}\n") ###
+
+        # breaking the loop if no progress, probably too many unknowns to have one version of solution/incorrect input
+        if prev_zeros == zeros:
+            # print(f"No progress, number of unknowns: {zeros}, incorrect input") ###
+            messagebox.showerror(title="Incorrect input", message="Too many unknowns")
+            break
+
+        round_counter = round_counter + 1
+
+    # solution printing and converting into 2d list
+    # print(f"Puzzle solved:\n{grid}") ###
+    solved_list = list(grid)   # why not: solved_list = grid.tolist() ? as np array: solved_list = grid also works
     solved_str = '\n'.join(' '.join(str(x) for x in row) for row in solved_list)
+    print(solved_str)
     solved_lab = tk.Label(bot_frame, text=solved_str, font=('Arial', 18))
     solved_lab.grid(row=11, column=0, columnspan=9, padx=5, pady=5)
-    if no_progress == 1:
-        messagebox.showerror(title="Incorrect input", message="Too many unknowns")
 
 
 def on_closing():
+    """
+    Function showing messagebox with question about exit, activated after pressing exit button.
+    """
+
+    # messagebox before exit
     if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
         root.destroy()
 
 
+# tkinter window launch and main settings
 root = tk.Tk()
 
 root.geometry("600x700+500+50")
@@ -290,12 +302,15 @@ bot_frame.grid(row=1, column=0)
 top_label = tk.Label(top_frame, text="What do you want to do?", font=('Arial', 18))
 top_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
-random_btn = tk.Button(top_frame, text="Show random puzzle", width=20, height=1, font=('Arial', 18), command=show_random)
+# definition of 2 first buttons in the window
+random_btn = tk.Button(top_frame, text="Show random puzzle", width=20, height=1, font=('Arial', 18),
+                       command=random_puzzle)
 random_btn.grid(row=1, column=0, padx=5, pady=5)
 
-own_btn = tk.Button(top_frame, text="Input your puzzle", width=20, height=1, font=('Arial', 18), command=input_own)
+own_btn = tk.Button(top_frame, text="Input your puzzle", width=20, height=1, font=('Arial', 18), command=own_puzzle)
 own_btn.grid(row=1, column=1, padx=5, pady=5)
 
+# action during exit
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
